6902491b5372c68980da85be34f18ec4e6cc726c
LuizzP <luizz.paullo@gmail.com>
2021-04-15 23:32:22 -0400

    modulos no browser - onload init

---
+++ mod07/modulos-browser/main.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    console.log('foiiiiii')
+    const btn = document.getElementById('btnCalcular')
+    btn.onclick = () => {
+        console.log('clicou')
+    }
+}
---
+++ mod07/modulos-browser/matematica.js
@@ -0,0 +1,12 @@
+class Matematica {
+    static somar(valor1, valor2) {
+        return valor1 + valor2
+    }
+
+    static multiplicar(valor1, valor2) {
+        return valor1 * valor2
+    }
+}
+
+// ja fica publico sozinho nao precisa do module export
+//so adicionar no HTML  
