081f697a611a009b396014089e5f6be8ea5adafa
LuizzP <luizz.paullo@gmail.com>
2021-04-15 23:48:03 -0400

    finished

--- mod07/modulos-browser/main.js
+++ mod07/modulos-browser/main.js
@@ -1,7 +1,25 @@
 window.onload = () => {
     console.log('foiiiiii')
     const btn = document.getElementById('btnCalcular')
-    btn.onclick = () => {
-        console.log('clicou')
-    }
+    btn.onclick = click 
+        function obterValorInput(id) {
+            const item = document.getElementById(id)
+            return item.value
+        }
+
+        function click(){
+            const tipoOperacao = obterValorInput('tipoOperacao')
+            const valor1 = obterValorInput('valor1')
+            const valor2 = obterValorInput('valor2')
+            const resultado = Matematica[tipoOperacao] (
+                valor1, valor2
+            )
+
+            console.log('resultado', resultado)
+            document.getElementById('resultado')
+            .innerText =`
+                A Operacao de ${tipoOperacao}, ${valor1} por ${valor2}: ${resultado}
+            `
+        }
+
 }
--- mod07/modulos-browser/matematica.js
+++ mod07/modulos-browser/matematica.js
@@ -1,6 +1,6 @@
 class Matematica {
     static somar(valor1, valor2) {
-        return valor1 + valor2
+        return Number(valor1) + Number(valor2)
     }
 
     static multiplicar(valor1, valor2) {
